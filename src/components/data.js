1b2e7a777a87880779dfd654e1a25ac883ee2a86
kayliewambui <kayliewambui20@gmail.com>
2024-05-03 15:53:12 +0300

    update files

--- src/components/data.js
+++ src/components/data.js
@@ -2,7 +2,14 @@ import { type } from "@testing-library/user-event/dist/type";
 import { useState,useEffect } from "react";
 
 const Data = () =>{
-   
+    const [data,setData] = useState([]);
+    
+    useEffect(()=>{
+        fetch("http://localhost:3000/")
+        .then((res)=>res.json())
+        .then((data)=>setData(data))
+    },[])
+   console.log(data)
     return(
         <>
             <h1>Hello Data</h1>
